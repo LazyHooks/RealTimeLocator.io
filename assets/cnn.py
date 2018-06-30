c8f79e60305f7da3b76cecf6d177bba2013373a8
Mainak Roy <mainakroy.cs@gmail.com>
2018-06-30 11:28:23 +0530

    mod cnn

---
+++ assets/cnn.py
@@ -0,0 +1,92 @@
+#imports0
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+#cnn initialisation
+classifier = Sequential()
+
+#convolution layer
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+
+#pooling layer
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+#level 2 conv and pooling
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+#flattening
+classifier.add(Flatten())
+
+#full connection layers
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+#compilation
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+#fitting CNN to data
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 8000,
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 2000)
+
+#save model
+classifier.save('image_classifier.h5') 
+
+#load model when necessary
+from keras.models import load_model
+classifier = load_model('image_classifier.h5')
+
+
+
+#single prediction
+
+#preprocess image
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+
+#predict
+result = classifier.predict(test_image)
+if result[0][0]==1:
+    prediction = 'canine'
+else:
+    prediction = 'feline'
+
+
+
+
+
+
+
+
+
+
+
