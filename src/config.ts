52d1f28b627cd4d8f916c817d058f1c6833d696f
KUNAL1612 <kunalchakrabarty@gmail.com>
2018-06-30 23:45:44 +0530

    addon_v0

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+	fire: {
+        apiKey: "AIzaSyChxDH65G3mpUmASUpzC8yd0fe9kyi7sPE",
+        authDomain: "angel-hack-app-auth.firebaseapp.com",
+        databaseURL: "https://angel-hack-app-auth.firebaseio.com",
+        projectId: "angel-hack-app-auth",
+        storageBucket: "angel-hack-app-auth.appspot.com",
+        messagingSenderId: "826915340146"
+	}
+};
