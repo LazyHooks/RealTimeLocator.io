73ef7dbb685e13baeda0b21022f8309409d8409c
Mainak Roy <mainakroy.cs@gmail.com>
2018-06-30 18:44:27 +0530

    NNs_v1

--- src/NNs/cnn.py
+++ src/NNs/cnn.py
@@ -69,16 +69,16 @@ classifier = load_model('image_classifier.h5')
 #preprocess image
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.load_img('dataset/single_prediction/test.jpg, target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 
 #predict
 result = classifier.predict(test_image)
 if result[0][0]==1:
-    prediction = 'canine'
+    prediction = 'sfw'
 else:
-    prediction = 'feline'
+    prediction = 'nsfw'
 
 
 
